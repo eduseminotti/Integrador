374d1831c4f1a4577ae11945a4d0c763c4dad953
eduseminotti <eduseminotti@gmail.com>
2019-10-22 18:36:19 -0300

    commit

--- Projeto/venv/BancoDB.py
+++ Projeto/venv/BancoDB.py
@@ -3,11 +3,11 @@ import pymssql
 class Banco():
 
     def __init__(self):
-        host = "DESKTOP-L112HN3"
+        host = "demotmsql\demonstracao"
         user = "sa"
-        password = "123456"
-        db = "seminotti"
-        self.conexao = pymssql.connect(host, user, password, db) 
+        password = "P@ssw0rd!"
+        db = "Seminotti_Teste"
+        self.conexao = pymssql.connect(host, user, password, db)
 
 
         
