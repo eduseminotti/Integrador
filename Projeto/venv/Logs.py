49b7f289e28caf8b660c4728deb02d04d219ce58
eduseminotti <eduseminotti@gmail.com>
2019-10-30 21:52:49 -0300

    commit

---
+++ Projeto/venv/Logs.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, render_template , redirect , url_for , request ,session
+from pathlib import Path
+from datetime import datetime
+
+class Logs(object):
+
+    def __init__(self, tipo="", message=""):
+        self.info = {}
+        self.tipo = tipo
+        self.message = message
+
+        self.fileName = "c:\log.txt"
+
+    def CriaArqCasoNaoExista(self):
+   
+        file = Path(self.fileName)
+        isFile = file.is_file()
+
+
+        if isFile == True :
+            return
+        else:
+            arquivo = open(self.fileName, 'w')
+            arquivo.close()
+
+                
+    def LogadorErro(self, message):
+
+        self.message = message
+
+        self.CriaArqCasoNaoExista()
+        
+        now = datetime.now()
+        now = now.strftime("%d/%m/%Y (%H:%M:%S.%f)")
+
+        arq = open(self.fileName, "r")
+        conteudo = arq.readlines() 
+        conteudo.append("\n" + now + " - [ERRO] - " + self.message )
+
+        arq = open(self.fileName, "w")
+
+        arq.writelines(conteudo)
+        arq.close()  
+
+    def LogadorInfo(self,message):
+
+        self.message = message
+
+        self.CriaArqCasoNaoExista()
+        
+        now = datetime.now()
+        now = now.strftime("%d/%m/%Y (%H:%M:%S.%f)")
+
+        arq = open(self.fileName, "r")
+        conteudo = arq.readlines() 
+        conteudo.append("\n" + now + " - [INFO] - " + self.message )
+
+        arq = open(self.fileName, "w")
+
+        arq.writelines(conteudo)
+        arq.close()           
+
+
+
+                    
+
+
+
